469ec202d936e9ac624093a5273bc42c1fb1f812
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-20 02:02:47 +0300

    Basic Server listening setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at ${process.env.PORT}`);
+});
