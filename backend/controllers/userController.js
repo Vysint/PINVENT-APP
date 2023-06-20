3a76301ffd4c3870e7341dbec1fbe53c3b91d68b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-20 23:19:41 +0300

    Register User POST request

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,59 @@
 const User = require("../models/userModel");
 
+exports.registerUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-exports.registerUser = async(req, res, next) => {
-  
-}
+  // Validation
+  try {
+    if (!name || !email || !password) {
+      res.status(401);
+      throw new Error("Please fill all required fields");
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  // Password Check
+
+  try {
+    if (password.length < 6) {
+      res.status(400);
+      throw new Error("Password must be upto 6 characters");
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  // Check if user exists
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(401);
+      throw new Error("Email has already been registered");
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  // Create a new User
+
+  try {
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json({
+      _id: savedUser._id,
+      name: savedUser.name,
+      email: savedUser.email,
+      photo: savedUser.photo,
+      phone: savedUser.phone,
+      bio: savedUser.bio,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
