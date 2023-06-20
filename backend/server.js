bfb00e4705ce36ebc58eebbd80f18915fb09695e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-20 12:31:34 +0300

    Connected to mongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 dotenv.config();
 
+// Connect to mongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connect to mongoDB!`);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
+};
+
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at ${process.env.PORT}`);
+  connect();
 });
