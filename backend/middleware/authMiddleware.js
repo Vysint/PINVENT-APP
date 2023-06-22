4ecaa0c76775d05e0a2afd90470ef8bac3a6e05d
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-23 01:19:01 +0300

    Token verification middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/userModel");
+
+exports.protect = async (req, res, next) => {
+  let token = req.cookies.jwt;
+
+  try {
+    if (token) {
+      try {
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        next();
+      } catch (err) {
+        return next(err);
+      }
+    } else {
+      res.status(400);
+      throw new Error("Not authorized, please login");
+    }
+  } catch (err) {
+    return next(err);
+  }
+};
