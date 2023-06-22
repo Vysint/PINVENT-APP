bc5f3ff899e78b03b56913e23f5fb5f596add10f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-22 22:58:44 +0300

    Added cors() middleware

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cookieParser());
+app.use(cors());
 
 // Routes Middlewares
 app.use("/api/users", userRoutes);
